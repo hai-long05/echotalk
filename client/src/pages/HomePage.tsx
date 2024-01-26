b0d68b128a0613c0755be57aeec9a835fb680bb5
Pham Hai Long Do <hai.long.do@sap.com>
2024-01-26 11:45:21 +0100

    rework design

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,17 +1,8 @@
-import { useState } from 'react';
-import Contacts from '../components/Contacts'
-import Sidenav from '../components/Sidenav'
-import AddContactModal from '../components/AddContactModal';
-
 const HomePage = () => {
 
-  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
     <div className='w-screen h-screen flex flex-row bg-bg-color'>
-      <Sidenav  setIsOpen={setIsOpen}/>
-      <Contacts />
-      <AddContactModal isOpen={isOpen}/>
     </div>
   )
 }
--- client/src/utils/ProtectedRoute.tsx
+++ client/src/utils/ProtectedRoute.tsx
@@ -5,7 +5,6 @@ import { Navigate, Outlet } from 'react-router-dom'
 const ProtectedRoute = () => {
 
   const auth = useAuth()
-
   if (!auth?.currentUser) return <Navigate to='/auth' replace />
 
   return <Outlet />
